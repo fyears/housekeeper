a105306e27c08f268f76b3f8bf321012cac21e74
fyears <hjwdhjwd@gmail.com>
2018-05-20 00:05:40 +0800

    check secret header

--- housekeeper/index.py
+++ housekeeper/index.py
@@ -3,6 +3,8 @@
 from __future__ import unicode_literals, print_function
 import os
 import sys
+from hashlib import sha1
+import hmac
 
 from flask import Flask, jsonify, request
 
@@ -11,6 +13,7 @@ from .reaction import Reaction
 try:
     user = os.environ['GITHUB_USER']
     password = os.environ['GITHUB_PASSWORD']
+    secret_key = os.environ.get('GITHUB_SECRET', None) # maybe not enabled
     posts_location = os.environ.get('POSTS_LOCATION', u'content/post/')
 except KeyError:
     sys.stderr.write(u'you need to specify env var'
@@ -40,8 +43,31 @@ def webhook():
 
     event = request.headers.get('X-GitHub-Event', None)
     # delivery = request.headers.get('X-GitHub-Delivery', None)
-    # signature = request.headers.get('X-Hub-Signature', None)
+    signature = request.headers.get('X-Hub-Signature', None)
     # user_agent = request.headers.get('User-Agent', None)
+
+    if secret_key is not None and secret_key: # ignore meaningless empty secret
+        # then we do some basic verification
+        bad_request = jsonify({
+            'status': 'error',
+            'data': 'invalid request'
+        }), 400
+        if signature is None:
+            return bad_request
+        signature = signature[len('sha1='):]
+        try:
+            secret_key_bytes = secret_key.encode('utf-8')
+        except AttributeError:
+            secret_key_bytes = secret_key
+        computed = hmac.new(secret_key_bytes, request.data, sha1).hexdigest()
+        if sys.version_info >= (2, 7, 7):
+            if not hmac.compare_digest(str(signature), str(computed)):
+                return bad_request
+        else:
+            # old python version
+            if str(signature) != str(computed):
+                return bad_request
+
     try:
         payload = request.json
     except:
